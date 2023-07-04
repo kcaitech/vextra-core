748b46e2af13c870d393fd645603e1d0cf1e2a94
ruiqiangzh <ruiqiangzh@gmail.com>
2023-07-04 10:11:08 +0800

    fix bug & 整理代码

--- src/editor/frame.ts
+++ src/editor/frame.ts
@@ -71,8 +71,7 @@ function afterModifyGroupShapeWH(api: Api, page: Page, shape: GroupShape, scaleX
             const cFrame = c.frame;
             const boundingBox = c.boundingBox();
 
-            // matrix.preScale(cFrame.width, cFrame.height); // 当对象太小时，求逆矩阵会infinity
-            matrix.preScale(100, 100);
+            matrix.preScale(cFrame.width, cFrame.height); // 当对象太小时，求逆矩阵会infinity
             if (c.rotation) api.shapeModifyRotate(page, c, 0);
             if (c.isFlippedHorizontal) api.shapeModifyHFlip(page, c, !c.isFlippedHorizontal);
             if (c.isFlippedVertical) api.shapeModifyVFlip(page, c, !c.isFlippedVertical);
@@ -82,8 +81,7 @@ function afterModifyGroupShapeWH(api: Api, page: Page, shape: GroupShape, scaleX
             api.shapeModifyWH(page, c, boundingBox.width, boundingBox.height);
 
             const matrix2 = c.matrix2Parent();
-            // matrix2.preScale(boundingBox.width, boundingBox.height);
-            matrix2.preScale(100 * scaleX, 100 * scaleY);
+            matrix2.preScale(boundingBox.width, boundingBox.height);
 
             matrix.multiAtLeft(matrix2.inverse);
 
