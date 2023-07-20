24d8c434511176ddf225a2038fea7284205f538b
ruiqiangzh <ruiqiangzh@gmail.com>
2023-07-20 16:13:30 +0800

    table read

---
+++ src/editor/table.ts
@@ -0,0 +1,19 @@
+import { ShapeEditor } from "./shape";
+
+export class TableEditor extends ShapeEditor {
+    // 水平拆分单元格
+    horSplitCell() {
+
+    }
+    // 垂直拆分单元格
+    verSplitCell() {
+
+    }
+    // 合并单元格
+    mergeCells() {
+
+    }
+    // 调整行高
+    // 调整列宽
+
+}
