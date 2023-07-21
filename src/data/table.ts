64c050c6ea8d9e7ab475c5f25c0a3609cdb163ee
ruiqiangzh <ruiqiangzh@gmail.com>
2023-07-21 15:52:00 +0800

    table get path

--- src/data/table.ts
+++ src/data/table.ts
@@ -4,6 +4,7 @@ import { BasicArray } from "./basic";
 import { ShapeType, ShapeFrame } from "./baseclasses"
 import { ImageShape, Shape, TextShape } from "./shape";
 import { ColumSegment, RowSegment, getColumnsInfo, getRowsInfo } from "./tableread";
+import { Path } from "./path";
 export { ColumSegment, RowSegment } from "./tableread";
 
 export class TableCell extends Shape implements classes.TableCell {
@@ -47,6 +48,20 @@ export class TableShape extends Shape implements classes.TableShape {
             style
         )
         this.childs = childs
+    }
+    getPath(offsetX: number, offsetY: number): Path;
+    getPath(origin?: boolean): Path;
+    getPath(arg1?: boolean | number, arg2?: number): Path {
+        const x = typeof arg1 == "boolean" ? (arg1 ? 0 : this.frame.x) : (arg1 as number);
+        const y = typeof arg1 == "boolean" ? (arg1 ? 0 : this.frame.y) : (arg2 as number);
+        const w = this.frame.width;
+        const h = this.frame.height;
+        const path = [["M", x, y],
+        ["l", w, 0],
+        ["l", 0, h],
+        ["l", -w, 0],
+        ["z"]];
+        return new Path(path);
     }
 
     getColumnsInfo(): ColumSegment[][] {
