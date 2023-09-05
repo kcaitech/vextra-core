792ad0346dfc00a98ccbc69f903bfb722b6c9077
Zrx <georgezrx@163.com>
2023-09-05 11:57:39 +0800

    new render for contact

---
+++ src/render/contact.ts
@@ -0,0 +1,40 @@
+import { Shape } from "../data/classes";
+import { render as renderB } from "./contact_borders";
+
+export function render(h: Function, shape: Shape, reflush?: number) {
+    const isVisible = shape.isVisible ?? true;
+    if (!isVisible) return;
+
+    const frame = shape.frame;
+    const props: any = {}
+    if (reflush) props.reflush = reflush;
+
+    const contextSettings = shape.style.contextSettings;
+    if (contextSettings && (contextSettings.opacity ?? 1) !== 1) {
+        props.opacity = contextSettings.opacity;
+    }
+
+    if (shape.isFlippedHorizontal || shape.isFlippedVertical || shape.rotation) {
+        const cx = frame.x + frame.width / 2;
+        const cy = frame.y + frame.height / 2;
+        const style: any = {}
+        style.transform = "translate(" + cx + "px," + cy + "px) "
+        if (shape.isFlippedHorizontal) style.transform += "rotateY(180deg) "
+        if (shape.isFlippedVertical) style.transform += "rotateX(180deg) "
+        if (shape.rotation) style.transform += "rotate(" + shape.rotation + "deg) "
+        style.transform += "translate(" + (-cx + frame.x) + "px," + (-cy + frame.y) + "px)"
+        props.style = style;
+    }
+    else {
+        props.transform = `translate(${frame.x},${frame.y})`
+    }
+    let childs = new Array();
+    if (shape.style.borders.length) {
+        const path = shape.getPath().toString();
+        childs = childs.concat(renderB(h, shape.style, path, shape));
+        return h('g', props, childs);
+    } else {
+        props.stroke = '#000000', props['stroke-width'] = 1, props.d = shape.getPath().toString();
+        return h('path', props);
+    }
+}
---
+++ src/render/contact_borders.ts
@@ -0,0 +1,57 @@
+
+
+import { Border, FillType, MarkerType, Shape, Style } from "../data/classes";
+import { render as ra } from "./apex2";
+function getHorizontalRadians(A: { x: number, y: number }, B: { x: number, y: number }) {
+    return Math.atan2(B.y - A.y, B.x - A.x)
+}
+function handler(h: Function, style: Style, border: Border, path: string, shape: Shape, startMarkerType?: MarkerType, endMarkerType?: MarkerType): any {
+    const thickness = border.thickness;
+    const body_props: any = {
+        d: path,
+        fill: "none",
+        stroke: '',
+        'stroke-width': thickness
+    }
+    const { length, gap } = border.borderStyle;
+    if (length || gap) body_props['stroke-dasharray'] = `${length}, ${gap}`;
+    const fillType = border.fillType;
+    if (fillType === FillType.SolidColor) {
+        const color = border.color;
+        const opacity = style.contextSettings?.opacity || 1;
+        body_props.stroke = "rgba(" + color.red + "," + color.green + "," + color.blue + "," + (color.alpha * opacity) + ")";
+    }
+    if (endMarkerType !== MarkerType.Line || startMarkerType !== MarkerType.Line) {
+        const f = shape.frame, t = border.thickness;
+        let s = { x: 0, y: 0 }, e = { x: f.width, y: f.height };
+        const r = getHorizontalRadians(s, e);
+        const g_cs: any[] = ra(h, style, f, border, r, startMarkerType, endMarkerType);
+        if (startMarkerType && startMarkerType !== MarkerType.Line && startMarkerType !== MarkerType.OpenArrow && startMarkerType !== MarkerType.Round && startMarkerType !== MarkerType.Square) {
+            s.x = 2 * t * Math.cos(r), s.y = 2 * t * Math.sin(r);
+        }
+        if (endMarkerType && endMarkerType !== MarkerType.Line && endMarkerType !== MarkerType.OpenArrow && endMarkerType !== MarkerType.Round && endMarkerType !== MarkerType.Square) {
+            e.x -= 2 * t * Math.cos(r), e.y -= 2 * t * Math.sin(r);
+        }
+        body_props.d = `M ${s.x} ${s.y} L ${e.x} ${e.y}`;
+        g_cs.push(h('path', body_props));
+        return g_cs;
+    } else {
+        return h('path', body_props);
+    }
+}
+
+
+export function render(h: Function, style: Style, path: string, shape: Shape): Array<any> {
+    const bc = style.borders.length;
+    let elArr = new Array();
+    const sm = style.startMarkerType, em = style.endMarkerType;
+    for (let i = 0; i < bc; i++) {
+        const border: Border = style.borders[i];
+        if (!border.isEnabled) continue;
+        const fillType = border.fillType;
+        (fillType === FillType.SolidColor) && (() => {
+            elArr = elArr.concat(handler(h, style, border, path, shape, sm, em));
+        })()
+    }
+    return elArr;
+}
