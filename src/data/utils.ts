b5146ac551ca092fd58e9e7a19018c41c3eabf16
Zrx <georgezrx@163.com>
2023-09-11 10:52:50 +0800

    整理代码

--- src/data/utils.ts
+++ src/data/utils.ts
@@ -17,7 +17,9 @@ interface PageXY {
     x: number
     y: number
 }
-
+/**
+ * @description 根据连接类型获取页面坐标系上的连接点
+ */
 function get_pagexy(shape: Shape, type: ContactType, m2r: Matrix) {
     const f = shape.frame;
     switch (type) {
@@ -33,9 +35,9 @@ function get_nearest_border_point(shape: Shape, contactType: ContactType, m2r: M
     const points = [{ x: 0, y: 0 }, { x: f.width, y: 0 }, { x: f.width, y: f.height }, { x: 0, y: f.height }];
     const t = m2r.computeCoord2(0, 0);
     const box = { left: t.x, right: t.x, top: t.y, bottom: t.y };
+    const offset = AStar.OFFSET;
     for (let i = 1; i < 4; i++) {
-        const p = points[i];
-        const t = m2r.computeCoord2(p.x, p.y);
+        const p = points[i], t = m2r.computeCoord3(p);
         if (t.x < box.left) {
             box.left = t.x;
         } else if (t.x > box.right) {
@@ -47,7 +49,7 @@ function get_nearest_border_point(shape: Shape, contactType: ContactType, m2r: M
             box.bottom = t.y
         }
     }
-    box.left -= 20, box.right += 20, box.top -= 20, box.bottom += 20;
+    box.left -= offset, box.right += offset, box.top -= offset, box.bottom += offset;
     let op = get_pagexy(shape, contactType, m2r);
     if (op) {
         const d1 = Math.abs(op.y - box.top);
@@ -75,52 +77,52 @@ function XYsBoundingPoints(points: PageXY[]) {
     const xs: number[] = [];
     const ys: number[] = [];
     for (let i = 0; i < points.length; i++) {
-        xs.push(points[i].x);
-        ys.push(points[i].y);
+        const p = points[i];
+        xs.push(p.x), ys.push(p.y);
     }
     const top = Math.min(...ys);
     const bottom = Math.max(...ys);
     const left = Math.min(...xs);
     const right = Math.max(...xs);
     return [
-        { x: left, y: top },
-        { x: right, y: top },
-        { x: right, y: bottom },
-        { x: left, y: bottom },
+        { x: left, y: top }, // lt
+        { x: right, y: top }, // rt
+        { x: right, y: bottom }, // rb
+        { x: left, y: bottom }, // lb
     ];
 }
+/**
+ * @description 一定误差范围内的相等判定
+ */
 function isEqu(a: number, b: number) {
     return Math.abs(a - b) < 0.0001;
 }
+/**
+ * @description 获取两条线的焦点
+ */
 function get_intersection(line1: [PageXY, PageXY], line2: [PageXY, PageXY]) {
-    if (isEqu(line1[0].x, line1[1].x) && isEqu(line2[0].x, line2[1].x)) {
-        return false;
-    }
-    if (isEqu(line1[0].y, line1[1].y) && isEqu(line2[0].y, line2[1].y)) {
-        return false;
-    }
-    if (isEqu(line1[0].y, line1[1].y) && isEqu(line2[0].x, line2[1].x)) {
-        return { x: line2[0].x, y: line1[0].y };
-    }
-    if (isEqu(line1[0].x, line1[1].x) && isEqu(line2[0].y, line2[1].y)) {
-        return { x: line1[0].x, y: line2[0].y };
-    }
-};
-function removeDuplicatePoint(points: PageXY[]) {
-    const res: PageXY[] = [];
-    const cache: any = {};
+    if (isEqu(line1[0].x, line1[1].x) && isEqu(line2[0].x, line2[1].x)) return false;
+    if (isEqu(line1[0].y, line1[1].y) && isEqu(line2[0].y, line2[1].y)) return false;
+    if (isEqu(line1[0].y, line1[1].y) && isEqu(line2[0].x, line2[1].x)) return { x: line2[0].x, y: line1[0].y };
+    if (isEqu(line1[0].x, line1[1].x) && isEqu(line2[0].y, line2[1].y)) return { x: line1[0].x, y: line2[0].y };
+}
+/**
+ * @description 去除重复点
+ */
+function remove_duplicate_point(points: PageXY[]) {
+    const result: PageXY[] = [], cache: any = {};
     for (let i = 0, len = points.length; i < len; i++) {
         const { x, y } = points[i];
-        if (cache[`${x}_${y}`]) {
-            continue;
-        } else {
-            cache[`${x}_${y}`] = true;
-            res.push(points[i]);
-        }
+        if (cache[`${x}_${y}`]) continue;
+        result.push(points[i]);
+        cache[`${x}_${y}`] = true;
     }
-    return res;
-};
+    return result;
+}
 
+/**
+ * @description 生成寻路计算的必要参数，其中点位是关键
+ */
 export function gen_baisc_params(shape1: Shape, type1: ContactType, shape2: Shape, type2: ContactType, m1: Matrix, m2: Matrix) {
     const OFFSET = 20;
     const f1 = shape1.frame, f2 = shape2.frame;
@@ -133,45 +135,44 @@ export function gen_baisc_params(shape1: Shape, type1: ContactType, shape2: Shap
     const start_point = get_pagexy(shape1, type1, m1), end_point = get_pagexy(shape2, type2, m2);
     if (!start_point || !end_point) return false;
     let preparation_point: PageXY[] = [];
-    const fake_start_point1 = get_nearest_border_point(shape1, type1, m1);
-    const fake_start_point2 = get_nearest_border_point(shape2, type2, m2);
-    if (!fake_start_point1 || !fake_start_point2) return false;
+    const b_start_point = get_nearest_border_point(shape1, type1, m1);
+    const b_end_point = get_nearest_border_point(shape2, type2, m2);
+    if (!b_start_point || !b_end_point) return false;
 
-    preparation_point.push(fake_start_point1, fake_start_point2); // 获取伪起点和伪终点,并将它们添加到数组里
+    preparation_point.push(b_start_point, b_end_point); // 获取伪起点和伪终点,并将它们添加到数组里
 
     const t1 = { x: s1xy1.x - OFFSET, y: s1xy1.y - OFFSET }, t2 = { x: s1xy2.x + OFFSET, y: s1xy2.y + OFFSET };
-    preparation_point.push(...XYsBoundingPoints([fake_start_point1, fake_start_point2, t1, t2])); // 伪起点和伪终点形成的矩形 和 起点元素包围框 组成一个大矩形 的四个顶点
+    preparation_point.push(...XYsBoundingPoints([b_start_point, b_end_point, t1, t2])); // 伪起点和伪终点形成的矩形 和 起点元素包围框 组成一个大矩形 的四个顶点
 
     const t3 = { x: s2xy1.x - OFFSET, y: s2xy1.y - OFFSET }, t4 = { x: s2xy2.x + OFFSET, y: s2xy2.y + OFFSET };
-    preparation_point.push(...XYsBoundingPoints([fake_start_point1, fake_start_point2, t3, t4])); // 伪起点和伪终点形成的矩形 和 终点元素包围框 组成一个大矩形 的四个顶点
+    preparation_point.push(...XYsBoundingPoints([b_start_point, b_end_point, t3, t4])); // 伪起点和伪终点形成的矩形 和 终点元素包围框 组成一个大矩形 的四个顶点
 
-    const t5 = get_intersection([start_point, fake_start_point1], [end_point, fake_start_point2]);
+    const t5 = get_intersection([start_point, b_start_point], [end_point, b_end_point]);
     if (t5) preparation_point.push(t5);
     if (!t5) {
-        const t6 = { x: fake_start_point2.x + OFFSET, y: fake_start_point2.y }
-        const t7 = get_intersection([start_point, fake_start_point1], [fake_start_point2, t6]);
+        const t7 = get_intersection([start_point, b_start_point], [b_end_point, { x: b_end_point.x + OFFSET, y: b_end_point.y }]);
         if (t7) preparation_point.push(t7);
 
-        const t8 = { x: fake_start_point2.x, y: fake_start_point2.y + OFFSET };
-        const t9 = get_intersection([start_point, fake_start_point1], [fake_start_point2, t8]);
+        const t9 = get_intersection([start_point, b_start_point], [b_end_point, { x: b_end_point.x, y: b_end_point.y + OFFSET }]);
         if (t9) preparation_point.push(t9);
 
-        const t10 = { x: fake_start_point1.x + OFFSET, y: fake_start_point1.y };
-        const t11 = get_intersection([end_point, fake_start_point2], [fake_start_point1, t10]);
+        const t11 = get_intersection([end_point, b_end_point], [b_start_point, { x: b_start_point.x + OFFSET, y: b_start_point.y }]);
         if (t11) preparation_point.push(t11);
 
-        const t12 = { x: fake_start_point1.x, y: fake_start_point1.y + OFFSET };
-        const t13 = get_intersection([end_point, fake_start_point2], [fake_start_point2, t12]);
+        const t13 = get_intersection([end_point, b_end_point], [b_end_point, { x: b_start_point.x, y: b_start_point.y + OFFSET }]);
         if (t13) preparation_point.push(t13);
     }
-    preparation_point = removeDuplicatePoint(preparation_point);
-    return { start_point, end_point, fake_start_point1, fake_start_point2, preparation_point, ff1, ff2 };
+    preparation_point = remove_duplicate_point(preparation_point);
+    return { start_point, end_point, b_start_point, b_end_point, preparation_point, ff1, ff2 };
 }
-function checkIsSamePoint(a: PageXY | null, b: PageXY | undefined) {
-    if (!a || !b) return false;
+/**
+ * @description 一定范围误差内，判定ab为同一个点
+ */
+function check_is_same_point(a: PageXY, b: PageXY) {
     return isEqu(a.x, b.x) && isEqu(a.y, b.y);
 }
 interface AP {
+    id: string
     point: PageXY
     cost: number
     parent: AP | null
@@ -196,18 +197,15 @@ class AStar {
         this.startPoint = sp;
         this.endPoint = ep;
         this.pointList = ps;
-        // 存放待遍历的点
-        this.openList = [];
-        // 存放已经遍历的点
-        this.closeList = [];
+        this.openList = []; // 存放待遍历的点
+        this.closeList = [];  // 存放已经遍历的点
         this.shapeFrame1 = f1;
         this.shapeFrame2 = f2;
     }
-
-    // 算法主流程
-    start() {
+    run() {
         this.openList = [
             {
+                id: v4(),
                 point: this.startPoint, // 起点加入openList
                 cost: 0, // 代价
                 parent: null, // 父节点
@@ -215,33 +213,25 @@ class AStar {
         ];
         this.closeList = [];
         while (this.openList.length) {
-            // 在openList中找出优先级最高的点
-            const point = this.getBestPoint();
-            // point为终点，那么算法结束，输出最短路径
-            if (checkIsSamePoint(point.point, this.endPoint)) {
-                return this.getRoutes(point);
+            const point = this.most_advantageous_point();
+            if (check_is_same_point(point.point, this.endPoint)) {
+                return this.best_path(point);
             } else {
-                // 将point从openList中删除
-                this.removeFromOpenList(point);
-                // 将point添加到closeList中
+                this.remove_from_openlist(point); // 先将point从openList中删除，并推入closeList
                 this.closeList.push(point);
-                // 遍历point周围的点
-                const nextPoints: PageXY[] = this.getNextPoints(point.point, this.pointList) as PageXY[];
+                const nextPoints: PageXY[] = this.next_points(point.point, this.pointList) as PageXY[]; // 寻找下一个点
                 for (let i = 0; i < nextPoints.length; i++) {
-                    let cur = nextPoints[i];
+                    const cur = nextPoints[i];
                     // 如果该点在closeList中，那么跳过该点
-                    if (this.checkIsInList(cur, this.closeList)) {
-                        continue;
-                    } else if (!this.checkIsInList(cur, this.openList)) {
-                        // 如果该点也不在openList中
+                    if (this.is_exist_list(cur, this.closeList)) continue;
+                    if (!this.is_exist_list(cur, this.openList)) {
                         const pointObj: AP = {
+                            id: v4(),
                             point: cur,
-                            parent: point,// 设置point为当前点的父节点
+                            parent: point, // 设置point为下一个点的父节点
                             cost: 0,
                         };
-                        // 计算当前点的代价
-                        this.computeCost(pointObj);
-                        // 添加到openList中
+                        this.cost_assessment(pointObj); // 计算好代价之后推入openList
                         this.openList.push(pointObj);
                     }
                 }
@@ -251,9 +241,8 @@ class AStar {
     }
 
     // 获取openList中优先级最高的点，也就是代价最小的点
-    getBestPoint() {
-        let min = Infinity;
-        let point = this.openList[0];
+    most_advantageous_point() {
+        let min = Infinity, point = this.openList[0];
         for (let i = 0, len = this.openList.length; i < len; i++) {
             const item = this.openList[i];
             if (item.cost < min) {
@@ -265,66 +254,58 @@ class AStar {
     }
 
     // 从point出发，找出其所有祖宗节点，也就是最短路径
-    getRoutes(point: AP) {
-        let res = [point];
+    best_path(point: AP) {
+        const path_nodes = [point];
         let p = point.parent;
         while (p) {
-            res.unshift(p);
+            path_nodes.unshift(p);
             p = p.parent;
         }
-        return res.map((item) => item.point);
+        return path_nodes.map((item) => item.point);
     }
 
     // 将点从openList中删除
-    removeFromOpenList(point: AP) {
-        const index = this.openList.findIndex((item) => {
-            return checkIsSamePoint(point.point, item.point);
-        });
+    remove_from_openlist(point: AP) {
+        const index = this.openList.findIndex((item) => item.id === point.id);
         this.openList.splice(index, 1);
     }
 
     // 检查点是否在列表中
-    checkIsInList(point: PageXY, list: AP[]) {
-        return list.find((item) => {
-            return checkIsSamePoint(item.point, point);
-        });
+    is_exist_list(point: PageXY, list: AP[]) {
+        return list.find((item) => check_is_same_point(item.point, point));
     }
-    getNextPoints(point: PageXY, points: PageXY[]) {
+    next_points(point: PageXY, points: PageXY[]) {
         const { x, y } = point;
         const xSamePoints: PageXY[] = [];
         const ySamePoints: PageXY[] = [];
         // 找出x或y坐标相同的点
         for (let i = 0, len = points.length; i < len; i++) {
             const item = points[i];
-            if (checkIsSamePoint(point, item)) continue;
-            if (isEqu(item.x, x)) {
-                xSamePoints.push(item);
-            }
-            if (isEqu(item.y, y)) {
-                ySamePoints.push(item);
-            }
+            if (check_is_same_point(point, item)) continue;
+            if (isEqu(item.x, x)) xSamePoints.push(item);
+            if (isEqu(item.y, y)) ySamePoints.push(item);
         }
         // 找出x方向最近的点
-        const xNextPoints = this.getNextPoint(x, y, ySamePoints, "x", this.shapeFrame1, this.shapeFrame2);
+        const xNextPoints = this.next_point_d(x, y, ySamePoints, "x", this.shapeFrame1, this.shapeFrame2);
         // 找出y方向最近的点
-        const yNextPoints = this.getNextPoint(x, y, xSamePoints, "y", this.shapeFrame1, this.shapeFrame2);
+        const yNextPoints = this.next_point_d(x, y, xSamePoints, "y", this.shapeFrame1, this.shapeFrame2);
         return [...xNextPoints, ...yNextPoints];
     }
-    checkLineThroughElements(a: PageXY, b: PageXY, f1: ShapeFrameLike, f2: ShapeFrameLike) {
+    is_through(a: PageXY, b: PageXY, f1: ShapeFrameLike, f2: ShapeFrameLike) {
         let rects: ShapeFrameLike[] = [f1, f2];
         let minX = Math.min(a.x, b.x);
         let maxX = Math.max(a.x, b.x);
         let minY = Math.min(a.y, b.y);
         let maxY = Math.max(a.y, b.y);
-        const xxx = 0;
+        const offset = AStar.OFFSET - 2;
         // 水平线
         if (isEqu(a.y, b.y)) {
             for (let i = 0; i < rects.length; i++) {
                 let rect = rects[i];
-                if (minY >= rect.y - xxx &&
-                    maxY <= rect.y + rect.height + xxx &&
-                    minX <= rect.x + rect.width + xxx &&
-                    maxX >= rect.x - xxx) {
+                if (minY >= rect.y - offset &&
+                    maxY <= rect.y + rect.height + offset &&
+                    minX <= rect.x + rect.width + offset &&
+                    maxX >= rect.x - offset) {
                     return true;
                 }
             }
@@ -332,24 +313,24 @@ class AStar {
             // 垂直线
             for (let i = 0; i < rects.length; i++) {
                 let rect = rects[i];
-                if (minX >= rect.x - xxx &&
-                    maxX <= rect.x + rect.width + xxx &&
-                    minY <= rect.y + rect.height + xxx &&
-                    maxY >= rect.y - xxx) {
+                if (minX >= rect.x - offset &&
+                    maxX <= rect.x + rect.width + offset &&
+                    minY <= rect.y + rect.height + offset &&
+                    maxY >= rect.y - offset) {
                     return true;
                 }
             }
         }
         return false;
     }
-    getNextPoint(x: number, y: number, list: PageXY[], dir: 'x' | 'y', f1: ShapeFrameLike, f2: ShapeFrameLike) {
+    next_point_d(x: number, y: number, list: PageXY[], dir: 'x' | 'y', f1: ShapeFrameLike, f2: ShapeFrameLike) {
         const value = dir === "x" ? x : y;
         let nextLeftTopPoint = null;
         let nextRIghtBottomPoint = null;
         for (let i = 0; i < list.length; i++) {
             let cur = list[i];
             // 检查当前点和目标点的连线是否穿过起终点元素
-            if (this.checkLineThroughElements({ x, y }, cur, f1, f2)) continue;
+            if (this.is_through({ x, y }, cur, f1, f2)) continue;
             // 左侧或上方最近的点
             if (cur[dir] < value) {
                 if (nextLeftTopPoint) {
@@ -367,24 +348,22 @@ class AStar {
                 }
             }
         }
-        return [nextLeftTopPoint, nextRIghtBottomPoint].filter((point) => {
-            return !!point;
-        });
+        return [nextLeftTopPoint, nextRIghtBottomPoint].filter((point) => !!point);
     };
     // 计算一个点的代价
-    computeCost(point: AP) {
-        point.cost = this.computedGCost(point) + this.computedHCost(point);
+    cost_assessment(point: AP) {
+        point.cost = this.g_cost(point) + this.h_cost(point);
     }
-    computedGCost(point: AP) {
-        let res = 0;
+    g_cost(point: AP) {
+        let cost = 0;
         let par = point.parent;
         while (par) {
-            res += par.cost;
+            cost += par.cost;
             par = par.parent;
         }
-        return res;
+        return cost;
     }
-    computedHCost(point: AP) {
+    h_cost(point: AP) {
         return (
             Math.abs(this.endPoint!.x - point.point.x) +
             Math.abs(this.endPoint!.y - point.point.y)
@@ -394,9 +373,9 @@ class AStar {
 export function gen_path(shape1: Shape, type1: ContactType, shape2: Shape, type2: ContactType, m1: Matrix, m2: Matrix, m3: Matrix) {
     const params = gen_baisc_params(shape1, type1, shape2, type2, m1, m2);
     if (!params) return false;
-    const { start_point, end_point, fake_start_point1, fake_start_point2, preparation_point, ff1, ff2 } = params;
-    const aStar = new AStar(ff1, ff2, fake_start_point1, fake_start_point2, preparation_point);
-    let path = aStar.start();
+    const { start_point, end_point, b_start_point, b_end_point, preparation_point, ff1, ff2 } = params;
+    const aStar = new AStar(ff1, ff2, b_start_point, b_end_point, preparation_point);
+    let path = aStar.run();
     if (!path.length) return false;
     path = [start_point, ...path, end_point];
     const points: CurvePoint[] = [];
