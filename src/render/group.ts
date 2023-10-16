db9460db7e3fce70c5b95a47f86e1a242ed4c8b1
ruiqiangzh <ruiqiangzh@gmail.com>
2023-10-16 18:07:46 +0800

    fix bug

--- src/render/group.ts
+++ src/render/group.ts
@@ -159,13 +159,13 @@ export function renderGroupChilds3(h: Function, shape: Shape, childs: Array<Shap
         const target = m1.computeCoord(0, 0);
 
         const c_rotate = rotate + (cc.rotation || 0);
-        const c_hflip = hflip ? !!cc.isFlippedHorizontal : !cc.isFlippedHorizontal;
-        const c_vflip = vflip ? !!cc.isFlippedVertical : !cc.isFlippedVertical;
+        const c_hflip = hflip ? !cc.isFlippedHorizontal : !!cc.isFlippedHorizontal;
+        const c_vflip = vflip ? !cc.isFlippedVertical : !!cc.isFlippedVertical;
         const c_frame = cc.frame;
         // cc matrix2Parent
         const m2 = matrix2parent(c_frame.x, c_frame.y, c_frame.width, c_frame.height, c_rotate, c_hflip, c_vflip);
 
-        m2.trans(bbox.x, bbox.y);
+        m2.trans(parentFrame.x, parentFrame.y);
         const cur = m2.computeCoord(0, 0);
 
         const dx = target.x - cur.x;
