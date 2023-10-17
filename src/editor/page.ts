d395b68ac8ed8f7e913f3668dea0c4ad9a2c2927
Zrx <georgezrx@163.com>
2023-10-17 08:37:50 +0800

    0.2.11

--- src/editor/page.ts
+++ src/editor/page.ts
@@ -521,7 +521,6 @@ export class PageEditor {
                 result.push(parent.childs[index]);
                 index++;
             }
-            console.log('paster1')
             after_shapes_add_to_doc(api, this.__page, result);
             this.__repo.commit();
             return result;
