f4f1c68b274100385791dd1aa904167301205e6a
ruiqiangzh <ruiqiangzh@gmail.com>
2023-12-20 17:23:31 +0800

    线条

--- src/dataview/line.ts
+++ src/dataview/line.ts
@@ -1,7 +1,25 @@
 import { ShapeView } from "./shape";
-
+import { render as renderBorders } from "../render/line_borders"
+import { EL, elh } from "./el";
 export class LineView extends ShapeView {
     protected isNoSupportDiamondScale(): boolean {
         return true;
+    }
+
+    protected renderFills(): EL[] {
+        return [];
+    }
+
+    protected renderBorders(): EL[] {
+        if (this.m_data.style.borders.length > 0) {
+            return renderBorders(elh, this.m_data.style, this.getBorders(), this.getPathStr(), this.m_data);
+        } else {
+            const props: any = {};
+            props.stroke = '#000000';
+            props['stroke-width'] = 1;
+            props.d = this.getPathStr();
+            props.fill = "none"
+            return [elh('path', props)];
+        }
     }
 }
