f753e1de5ededb66f87191334dd0ee5e472e0abf
Zrx <georgezrx@163.com>
2023-12-27 15:57:10 +0800

    fix: symbol render

--- src/dataview/symbol.ts
+++ src/dataview/symbol.ts
@@ -1,5 +1,14 @@
 import { GroupShapeView } from "./groupshape";
+import { renderBorders, renderFills } from "../render";
+import { EL, elh } from "./el";
 
 export class SymbolView extends GroupShapeView {
-
+    // fills
+    protected renderFills(): EL[] {
+        return renderFills(elh, this.getFills(), this.frame, this.getPathStr());
+    }
+    // borders
+    protected renderBorders(): EL[] {
+        return renderBorders(elh, this.getBorders(), this.frame, this.getPathStr());
+    }
 }
