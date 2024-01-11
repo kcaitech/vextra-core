8e2e05a50e331015d8975ca37585c0fbc0006175
Zrx <georgezrx@163.com>
2024-01-11 16:13:37 +0800

    fix: contact line type

--- src/dataview/contactline.ts
+++ src/dataview/contactline.ts
@@ -3,8 +3,9 @@ import { ShapeView } from "./shape";
 import { render as renderBorders } from "../render/contact_borders"
 import { ContactForm, ContactShape, Page, Shape, ShapeType } from "../data/classes";
 import { DViewCtx, PropsType } from "./viewctx";
+import { PathShapeView } from "./pathshape";
 
-export class ContactLineView extends ShapeView {
+export class ContactLineView extends PathShapeView {
 
     private from: undefined | Shape;
     private to: undefined | Shape;
@@ -13,7 +14,7 @@ export class ContactLineView extends ShapeView {
     private page: Page | undefined = undefined;
 
     constructor(ctx: DViewCtx, props: PropsType) {
-        super(ctx, props, false);
+        super(ctx, props);
         this.wathcer_sides = this.wathcer_sides.bind(this);
 
         this.updateApex();
