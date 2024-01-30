522159355eb699df9f90b5e5486558ae18dd1e06
Zrx <georgezrx@163.com>
2024-01-30 17:23:37 +0800

    新建非第一级的容器，默认不填充颜色

--- src/editor/creator.ts
+++ src/editor/creator.ts
@@ -140,10 +140,6 @@ export function newArtboard(name: string, frame: ShapeFrame, style?: Style): Art
 
     if (style) {
         artboard.style = style;
-    } else {
-        const fillColor = new Color(1, 255, 255, 255);
-        const fill = new Fill(uuid(), true, FillType.SolidColor, fillColor);
-        artboard.style.fills.push(fill);
     }
 
     addCommonAttr(artboard);
