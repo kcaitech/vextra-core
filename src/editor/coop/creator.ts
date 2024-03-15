134a88321369eca3804b34245d161ac07a193225
ruiqiangzh <ruiqiangzh@gmail.com>
2024-03-15 11:22:09 +0800

    cmd分批执行

--- src/editor/coop/creator.ts
+++ src/editor/coop/creator.ts
@@ -1,24 +1,25 @@
 import { Op, OpType } from "../../coop/common/op";
 import { Document } from "../../data/document";
 import { CrdtArrayReopNode } from "./arraynode";
+import { RepoNodePath } from "./base";
 import { CrdtIdRepoNode } from "./idsetnode";
 import { ISave4Restore } from "./localcmd";
 import { CrdtShapeRepoNode } from "./shapenode";
 import { TextRepoNode } from "./textnode";
 
 export function nodecreator(document: Document, selection: ISave4Restore | undefined) {
-    return (op: Op) => {
+    return (parent: RepoNodePath, op: Op) => {
         switch (op.type) {
             case OpType.Array:
                 // text
-                return new TextRepoNode(document, selection);
+                return new TextRepoNode(parent, document, selection);
             case OpType.CrdtArr:
                 // array
-                return new CrdtArrayReopNode(document);
+                return new CrdtArrayReopNode(parent, document);
             case OpType.CrdtTree:
-                return new CrdtShapeRepoNode(document);
+                return new CrdtShapeRepoNode(parent, document);
             case OpType.Idset:
-                return new CrdtIdRepoNode(document);
+                return new CrdtIdRepoNode(parent, document);
             case OpType.None:
                 throw new Error("op none?");
         }
