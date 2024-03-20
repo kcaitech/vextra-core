df0c567466c8fd73c58858425f9091d0919b9bf1
ruiqiangzh <ruiqiangzh@gmail.com>
2024-03-20 09:15:00 +0800

    统一data与view的isVisible、isLocked的接口

--- src/editor/shape.ts
+++ src/editor/shape.ts
@@ -328,7 +328,7 @@ export class ShapeEditor {
     public toggleVisible() {
         // 实例图层
         this._repoWrap('toggleVisible', (api) => {
-            const isVisible = !this.shape.isVisible;
+            const isVisible = !this.view.isVisible;
             if (this.modifyVariable(VariableType.Visible, OverrideType.Visible, isVisible, api)) return;
             api.shapeModifyVisible(this.__page, this.shape, isVisible);
         })
@@ -336,7 +336,7 @@ export class ShapeEditor {
 
     public toggleLock() {
         this._repoWrap('toggleLock', (api) => {
-            const isLocked = !this.shape.isLocked;
+            const isLocked = !this.view.isLocked;
             if (this.modifyVariable(VariableType.Lock, OverrideType.Lock, isLocked, api)) return;
             api.shapeModifyLock(this.__page, this.shape, isLocked);
         });
