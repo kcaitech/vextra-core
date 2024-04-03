cb493c87bae3d341ec84c07226b204f494e48077
ruiqiangzh <ruiqiangzh@gmail.com>
2024-04-03 14:17:17 +0800

    figma

--- src/io/import/figma/loader.ts
+++ src/io/import/figma/loader.ts
@@ -4,6 +4,49 @@ import { IJSON, LoadContext } from "./basic";
 import { importPage, importRectShape } from "./shapeio";
 
 
+function updatePageFrame(p: Page) {
+    const pf = p.frame;
+    const cc = p.childs.length;
+    if (cc === 0) {
+        p.frame.x = 0
+        p.frame.y = 0
+        p.frame.width = 0
+        p.frame.height = 0
+        return;
+    }
+    const c = p.childs[0];
+    const cf = c.frame;
+    let l = cf.x, t = cf.y, r = l + cf.width, b = t + cf.height;
+    for (let i = 1; i < cc; i++) {
+        const c = p.childs[i];
+        const cf = c.frame;
+        const cl = cf.x, ct = cf.y, cr = cl + cf.width, cb = ct + cf.height;
+        l = Math.min(cl, l);
+        t = Math.min(ct, t);
+        r = Math.max(cr, r);
+        b = Math.max(cb, b);
+        // console.log("c", i, cf)
+    }
+    // console.log("pf", pf)
+    // console.log(l, t, r, b)
+    // pf.set(pf.x + l, pf.y + t, r - l, b - t);
+    pf.x = pf.x + l
+    pf.y = pf.y + t
+    pf.width = r - l
+    pf.height = b - t
+    // console.log(pf)
+
+    for (let i = 0; i < cc; i++) {
+        const c = p.childs[i];
+        const cf = c.frame;
+        cf.x = cf.x - l;
+        cf.y = cf.y - t;
+        // cf.width = cf.width;
+        // cf.height = cf.height;
+        // console.log("c", i, cf)
+    }
+}
+
 export function startLoader(file: IJSON, pages: IJSON[], document: Document) {
     // const __remote: LzData = lzdata;
     const __document: Document = document;
@@ -24,6 +67,7 @@ export function startLoader(file: IJSON, pages: IJSON[], document: Document) {
         const json: IJSON | undefined = pages.find(p => p.id === id);
         if (!json) return new Page(new BasicArray(), id, "", ShapeType.Page, new ShapeFrame(0, 0, 100, 100), new Style(new BasicArray(), new BasicArray(), new BasicArray()), new BasicArray());
         const page = importPage(ctx, json, importer);
+        updatePageFrame(page);
         return page;
     }
 
--- src/io/import/figma/shapeio.ts
+++ src/io/import/figma/shapeio.ts
@@ -1,21 +1,35 @@
 import { BasicArray } from "../../../data/basic";
-import { CurvePoint, RectShape, Shape, ShapeFrame, ShapeType } from "../../../data/shape";
+import { CurveMode, CurvePoint, RectShape, Shape, ShapeFrame, ShapeType } from "../../../data/shape";
 import { uuid } from "../../../basic/uuid";
 import { IJSON, ImportFun, LoadContext } from "./basic";
 import { Page, Style } from "../../../data/classes";
 
 export function importPage(ctx: LoadContext, data: IJSON, f: ImportFun): Page {
+    // const size = data['size']; // 没有
+    // const transform = data['transform'];
+    const visible = data['visible'];
     const frame = new ShapeFrame(0, 0, 100, 100);
     const style = new Style(new BasicArray(), new BasicArray(), new BasicArray());
     const childs: Shape[] = (data['childs'] || []).map((d: IJSON, i: number) => f(ctx, d, i));
     const shape = new Page(new BasicArray<number>(), data.id, data.name, ShapeType.Page, frame, style, new BasicArray<Shape>(...childs));
+    shape.isVisible = visible;
     return shape;
 }
 
 export function importRectShape(ctx: LoadContext, data: IJSON, f: ImportFun, index: number): RectShape {
-    const frame = new ShapeFrame(0, 0, 100, 100);
+    const size = data['size'];
+    const transform = data['transform'];
+    const frame = new ShapeFrame(transform.m02, transform.m12, size.x, size.y);
+    const visible = data['visible'];
     const style = new Style(new BasicArray(), new BasicArray(), new BasicArray());
     const points = new BasicArray<CurvePoint>();
+    const p1 = new CurvePoint([0] as BasicArray<number>, uuid(), 0, 0, CurveMode.Straight); // lt
+    const p2 = new CurvePoint([1] as BasicArray<number>, uuid(), 1, 0, CurveMode.Straight); // rt
+    const p3 = new CurvePoint([2] as BasicArray<number>, uuid(), 1, 1, CurveMode.Straight); // rb
+    const p4 = new CurvePoint([3] as BasicArray<number>, uuid(), 0, 1, CurveMode.Straight); // lb
+    points.push(p1, p2, p3, p4);
     const isClosed = true;
-    return new RectShape([index] as BasicArray<number>, uuid(), data['name'], ShapeType.Rectangle, frame, style, points, isClosed);
+    const shape = new RectShape([index] as BasicArray<number>, uuid(), data['name'], ShapeType.Rectangle, frame, style, points, isClosed);
+    shape.isVisible = visible;
+    return shape;
 }
