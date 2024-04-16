5b51fad99ad24e92f98b8ac3c2ae940a4d3198a2
Zrx <georgezrx@163.com>
2024-04-16 11:50:47 +0800

    对普通文本的宽高调整进行向上取整，避免出现浮点数

--- src/editor/utils/other.ts
+++ src/editor/utils/other.ts
@@ -50,7 +50,10 @@ export function fixTextShapeFrameByLayout(api: _Api, page: Page, shape: TextShap
             const layout = shape.getLayout();
             const fontsize = shape.text.attr?.fontSize ?? Text.DefaultFontSize;
             // expandTo(api as Api, page, shape, shape.frame.width, Math.max(fontsize, layout.contentHeight));
-            api.shapeModifyWH(page, _shape, shape.frame.width, Math.max(fontsize, layout.contentHeight));
+
+            const targetHeight = Math.ceil(Math.max(fontsize, layout.contentHeight));
+
+            api.shapeModifyWH(page, _shape, shape.frame.width, targetHeight);
             break;
         }
         case TextBehaviour.Flexible: {
@@ -59,7 +62,11 @@ export function fixTextShapeFrameByLayout(api: _Api, page: Page, shape: TextShap
             if (xOffset !== 0) api.shapeModifyX(page, _shape, _shape.frame.x + xOffset);
             // const fontsize = shape.text.attr?.fontSize ?? Text.DefaultFontSize;
             // api.shapeModifyWH(page, _shape, Math.max(fontsize, layout.contentWidth), Math.max(fontsize, layout.contentHeight));
-            api.shapeModifyWH(page, _shape, layout.contentWidth, layout.contentHeight);
+
+            const targetWidth = Math.ceil(layout.contentWidth);
+            const targetHeight = Math.ceil(layout.contentHeight);
+
+            api.shapeModifyWH(page, _shape, targetWidth, targetHeight);
             // expandTo(api as Api, page, shape, Math.max(fontsize, layout.contentWidth), Math.max(fontsize, layout.contentHeight));
             break;
         }
