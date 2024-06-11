a9c25c19a200a7e3184d238b04c8daad0035734d
zhangruiqiang <zhangruiqiang@kcaitech.com>
2024-06-11 16:47:45 +0800

    build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,46 +2,25 @@ import typescript from '@rollup/plugin-typescript';
 import {babel} from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
-
-export default [
-    {
-        input: 'src/index.ts',
-        output: {
-            dir: 'dist/browser',
+export default [{
+    input: 'src/index.ts',
+    output: [
+        {
+            dir: 'dist',
             format: 'es',
-            exports: 'auto', // 根据代码中的导出自动推断导出方式
             entryFileNames: 'index.js'
         },
-        plugins: [
-            terser(),
-            babel({babelHelpers: 'bundled'}),
-            typescript({
-                tsconfig: './tsconfig.json',
-                outDir: 'dist/browser',
-                declarationDir: 'dist/browser/types',
-            }),
-            json(),
-        ],
-        external: ['uuid', 'kiwi-schema', 'uzip'],
-    },
-    {
-        input: 'src/index.ts',
-        output: {
-            dir: 'dist/node',
+        {
+            dir: 'dist',
             format: 'cjs',
-            exports: 'auto', // 根据代码中的导出自动推断导出方式
             entryFileNames: 'index.cjs'
-        },
-        plugins: [
-            terser(),
-            babel({babelHelpers: 'bundled'}),
-            typescript({
-                tsconfig: './tsconfig.json',
-                outDir: 'dist/node',
-                declarationDir: 'dist/node/types',
-            }),
-            json(),
-        ],
-        external: ['uuid', 'kiwi-schema', 'uzip'],
-    },
-];
+        }
+    ],
+    plugins: [
+        json(),
+        typescript(),
+        babel({ babelHelpers: 'bundled' }),
+        terser(),
+    ],
+    external: ['uuid', 'kiwi-schema', 'uzip']
+}];
