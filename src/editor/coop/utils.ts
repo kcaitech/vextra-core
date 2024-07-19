decf59f1204e09c2af99e06f0c4a75ae3c071c35
Zrx <georgezrx@163.com>
2024-07-19 18:12:21 +0800

    【FEAT】MaskGroup

--- src/editor/coop/utils.ts
+++ src/editor/coop/utils.ts
@@ -138,7 +138,7 @@ export function updateShapesFrame(page: Page, shapes: Shape[], api: Api) {
         next.updated = true;
         next.changed = changed;
     }
-    page.__collect.notify('collect'); // 收集辅助线采用的关键点位
+    // page.__collect.notify('collect'); // 收集辅助线采用的关键点位
 }
 
 const imhdl: { [key: string]: (source: any, ctx?: IImportContext) => any } = {};
