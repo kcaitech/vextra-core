0ae8975d34c4e29a3857bc8226653a289645ce9e
zhangruiqiang <zhangruiqiang@kcaitech.com>
2024-07-26 10:05:57 +0800

    优化代码

--- src/dataview/shape.ts
+++ src/dataview/shape.ts
@@ -183,17 +183,19 @@ function frameContains(frame: ShapeFrame, x: number, y: number) {
     return x >= frame.x && x < (frame.x + frame.width) && y >= frame.y && y < (frame.y + frame.height);
 }
 
-function hitTest(shape: ShapeView, x: number, y: number, type: 'm_frame' | 'm_visibleFrame' | 'm_outerFrame', deep: boolean, ret: { shape: ShapeView, x: number, y: number }[]) {
-    if (frameContains(shape[type], x, y)) {
-        ret.push({ shape, x, y })
-        if (deep) {
-            for (let i = 0, len = shape.m_children.length; i < len; ++i) {
-                const child = shape.m_children[i] as ShapeView;
-                const xy = child.m_transform.inverseCoord(x, y);
-                hitTest(child, xy.x, xy.y, type, true, ret)
-            }
+function _hitTest(shape: ShapeView, x: number, y: number, type: '_p_frame' | '_p_visibleFrame' | '_p_outerFrame', depth: number, ret: { shape: ShapeView, x: number, y: number }[]) {
+    for (let i = 0, len = shape.m_children.length; i < len; ++i) {
+        const child = shape.m_children[i] as ShapeView;
+        if (frameContains(child[type], x, y)) {
+            const xy = child.m_transform.inverseCoord(x, y);
+            ret.push({ shape: child, x: xy.x, y: xy.y })
+            if (depth > 1) _hitTest(child, xy.x, xy.y, type, depth - 1, ret)
         }
     }
+}
+
+function hitTest(shape: ShapeView, x: number, y: number, type: { m: 'm_frame', p: '_p_frame' } | { m: 'm_visibleFrame', p: '_p_visibleFrame' } | { m: 'm_outerFrame', p: '_p_outerFrame' }, depth: number, ret: { shape: ShapeView, x: number, y: number }[]) {
+    if (depth > 0 && frameContains(shape[type.m], x, y)) _hitTest(shape, x, y, type.p, depth, ret);
 }
 
 export function updateFrame(frame: ShapeFrame, x: number, y: number, w: number, h: number): boolean {
@@ -216,9 +218,9 @@ export class ShapeView extends DataView {
     m_visibleFrame: ShapeFrame = new ShapeFrame(); // 对象内坐标系的大小
     m_outerFrame: ShapeFrame = new ShapeFrame(); // 对象内坐标系的大小
 
-    _p_frame: ShapeFrame = new ShapeFrame(); // 父级坐标系的大小 // 用于优化updateFrames
-    _p_visibleFrame: ShapeFrame = new ShapeFrame(); // 父级坐标系的大小 // 用于优化updateFrames
-    _p_outerFrame: ShapeFrame = new ShapeFrame(); // 父级坐标系的大小 // 用于优化updateFrames
+    _p_frame: ShapeFrame = new ShapeFrame(); // 父级坐标系的大小 // 用于优化updateFrames, hittest
+    _p_visibleFrame: ShapeFrame = new ShapeFrame(); // 父级坐标系的大小 // 用于优化updateFrames, hittest
+    _p_outerFrame: ShapeFrame = new ShapeFrame(); // 父级坐标系的大小 // 用于优化updateFrames, hittest
 
     m_fixedRadius?: number;
 
@@ -235,19 +237,19 @@ export class ShapeView extends DataView {
         this.m_fixedRadius = (shape as PathShape).fixedRadius; // rectangle
     }
 
-    hitContent(x: number, y: number, deep: boolean): { shape: ShapeView; x: number; y: number; }[] {
+    hitContent(x: number, y: number, depth: number): { shape: ShapeView; x: number; y: number; }[] {
         const ret: { shape: ShapeView, x: number, y: number }[] = [];
-        hitTest(this, x, y, "m_frame", deep, ret);
+        hitTest(this, x, y, { m: "m_frame", p: '_p_frame' }, depth, ret);
         return ret;
     }
-    hitVisible(x: number, y: number, deep: boolean): { shape: ShapeView; x: number; y: number; }[] {
+    hitVisible(x: number, y: number, depth: number): { shape: ShapeView; x: number; y: number; }[] {
         const ret: { shape: ShapeView, x: number, y: number }[] = [];
-        hitTest(this, x, y, "m_visibleFrame", deep, ret);
+        hitTest(this, x, y, { m: "m_visibleFrame", p: "_p_visibleFrame" }, depth, ret);
         return ret;
     }
-    hitOuter(x: number, y: number, deep: boolean): { shape: ShapeView; x: number; y: number; }[] {
+    hitOuter(x: number, y: number, depth: number): { shape: ShapeView; x: number; y: number; }[] {
         const ret: { shape: ShapeView, x: number, y: number }[] = [];
-        hitTest(this, x, y, "m_outerFrame", deep, ret);
+        hitTest(this, x, y, { m: "m_outerFrame", p: "_p_outerFrame" }, depth, ret);
         return ret;
     }
 
