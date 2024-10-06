eda54bc565c925eeaf44a7d62dd05217a4af60a9
zrx <georgezrx@163.com>
2024-10-06 23:48:58 +0800

    增加migrate场景🎬

--- src/editor/asyncApiHandler/transform/translate.ts
+++ src/editor/asyncApiHandler/transform/translate.ts
@@ -13,7 +13,7 @@ import {
     StackMode,
     StackPositioning,
 } from "../../../data";
-import {Transform as TransformRaw} from "../../../data";
+import { Transform as TransformRaw } from "../../../data";
 import { after_migrate, unable_to_migrate } from "../../utils/migrate";
 import { get_state_name, is_state } from "../../symbol";
 import { Api } from "../../../coop";
@@ -35,18 +35,21 @@ export type TidyUpInfo = {
     dir: boolean
 }
 
-interface TranslateBaseItem {
+export interface TranslateBaseItem {
     transformRaw: TransformRaw,
     transform: Transform;
     view: ShapeView;
 }
 
+export interface MigrateItem {
+    view: ShapeView;
+    toParent: ShapeView;
+    allowSameEnv?: boolean; // 允许在同一父级下migrate
+    index?: number;
+}
+
 export class Transporter extends AsyncApiCaller {
-    origin_envs = new Map<string, { shape: ShapeView, index: number }[]>();
-    origin_xy_envs = new Map<string, { shape: ShapeView, xy: { x: number, y: number } }[]>();
     need_layout_shape = new Set<string>();
-    except_envs: ShapeView[] = [];
-    current_env_id: string = '';
     prototype = new Map<string, Shape>()
     shapes: (Shape | ShapeView)[] = [];
 
@@ -86,95 +89,20 @@ export class Transporter extends AsyncApiCaller {
         }
     }
 
-    shortPaste(_ss: Shape[], actions: { parent: GroupShape, index: number }[]) {
-        try {
-            const result: Shape[] = [];
-            for (let i = 0, len = actions.length; i < len; i++) {
-                const shape = _ss[i];
-                const {parent, index} = actions[i];
-                shape.name = this.genName(shape);
-                this.api.shapeInsert(this.__document, this.page, parent, shape, index);
-                result.push(parent.childs[index]);
-            }
-            this.updateView();
-            this.shapes = result;
-            return result;
-        } catch (error) {
-            console.log('Transporter.shortPaste:', error);
-            this.exception = true;
-            return false;
-        }
-    }
-
-    genName(shape: Shape) {
-        const reg = new RegExp('\\S+\\s[0-9]+$');
-        let name = shape.name;
-        if (reg.test(name)) {
-            const type = shape.type;
-            const index = name.lastIndexOf(' ');
-
-            const pre = name.slice(0, index);
-            const end = name.slice(index + 1);
-
-            let max = Number(end);
-            if (isNaN(max)) return name;
-
-            const reg2 = new RegExp(`^${pre}\\s[0-9]+$`);
-
-            this.page.shapes.forEach(s => {
-                if (s.type !== type) return;
-
-                if (!reg2.test(s.name)) return;
-
-                const index = s.name.lastIndexOf(' ');
-                const sEnd = Number(s.name.slice(index + 1));
-                if (isNaN(sEnd)) return;
-
-                if (sEnd > max) max = sEnd;
-            })
-            return `${pre} ${max + 1}`;
-        } else {
-            return name;
-        }
-    }
-
-    // migrate(targetParent: GroupShape, sortedShapes: Shape[], dlt: string) {
-    //     try {
-    //         if (targetParent.id === this.current_env_id) return;
-    //
-    //         const api = this.api;
-    //         const page = this.page;
-    //         const document = this.__document;
-    //
-    //         let index = targetParent.childs.length;
-    //         for (let i = 0, len = sortedShapes.length; i < len; i++) {
-    //             this.__migrate(document, api, page, targetParent, sortedShapes[i], dlt, index) && index++;
-    //         }
-    //         this.need_layout_shape.add(this.current_env_id);
-    //         const parents: GroupShape[] = [targetParent];
-    //         for (let i = 0; i < parents.length; i++) {
-    //             const parent = parents[i];
-    //             modifyAutoLayout(page, api, parent);
-    //         }
-    //
-    //         this.setCurrentEnv(targetParent);
-    //         this.updateView();
-    //     } catch (e) {
-    //         console.log('Transporter.migrate:', e);
-    //         this.exception = true;
-    //     }
-    // }
-    migrate(target: ShapeView, sortedShapes: ShapeView[], dlt: string) {
+    migrate(items: MigrateItem[], dlt: string) {
         try {
             const api = this.api;
             const page = this.page;
             const document = this.__document;
 
-            const targetParent = adapt2Shape(target) as GroupShape;
+            for (const item of items) {
+                if (item.toParent === item.view.parent && !item.allowSameEnv) continue;
 
-            let index = target.childs.length;
-            for (let i = 0, len = sortedShapes.length; i < len; i++) {
-                this.__migrate(document, api, page, targetParent, adapt2Shape(sortedShapes[i]), dlt, index) && index++;
+                const toParent = adapt2Shape(item.toParent) as GroupShape;
+                const shape = adapt2Shape(item.view);
+                const maxL = toParent.childs.length;
+                const index = Math.min(item.index ?? maxL, maxL);
+                this.__migrate(document, api, page, toParent, shape, dlt, index);
             }
 
             this.updateView();
@@ -187,65 +115,10 @@ export class Transporter extends AsyncApiCaller {
         }
     }
 
-    setEnv(envs: Map<string, { shape: ShapeView, index: number }[]>) {
-        this.origin_envs = envs;
-    }
-
-    setOriginXyEnv(envs: Map<string, { shape: ShapeView, xy: { x: number, y: number } }[]>) {
-        this.origin_xy_envs = envs;
-    }
-
-    setExceptEnvs(except: ShapeView[]) {
-        this.except_envs = except;
-    }
-
-    getExceptEnvs() {
-        return this.except_envs;
-    }
-
-    setCurrentEnv(cv: Shape | Page) {
-        this.current_env_id = cv.id;
-    }
-
-    backToStartEnv(emit_by: Shape, dlt: string) { // 特殊的migrate，让所有图层回到原环境
-        try {
-            if (emit_by.id === this.current_env_id) return;
-
-            const api = this.api;
-            const page = this.page;
-            const document = this.__document;
-            const __migrate = this.__migrate.bind(this);
-            this.origin_envs.forEach((v, k) => {
-                const op = page.getShape(k) as GroupShape;
-                if (!op) return;
-                for (let i = 0, l = v.length; i < l; i++) {
-                    const _v = v[i];
-                    this.need_layout_shape.delete(_v.shape.id);
-                    __migrate(document, api, page, op, adapt2Shape(_v.shape), dlt, _v.index);
-                }
-            });
-            this.origin_xy_envs.forEach((v, k) => {
-                const op = this.page.getShape(k) as GroupShape | undefined;
-                if (!op) return;
-
-                for (let i = 0, l = v.length; i < l; i++) {
-                    const _v = v[i];
-                    this.api.shapeModifyX(this.page, adapt2Shape(_v.shape), _v.xy.x);
-                    this.api.shapeModifyY(this.page, adapt2Shape(_v.shape), _v.xy.y);
-                }
-            });
-
-            this.updateView();
-            this.setCurrentEnv(emit_by);
-        } catch (error) {
-            console.log('Transporter.migrate', error);
-            this.exception = true;
-        }
-    }
-
     private __migrate(document: Document, api: Api, page: Page, targetParent: GroupShape, shape: Shape, dlt: string, index: number) {
         const error = unable_to_migrate(targetParent, shape);
         if (error) throw new MossError(`error type ${error}`);
+
         const origin: GroupShape = shape.parent as GroupShape;
         if (is_state(shape)) {
             const name = get_state_name(shape as any, dlt);
@@ -257,7 +130,7 @@ export class Transporter extends AsyncApiCaller {
         transform.addTransform(__t.getInverse());
 
         api.shapeModifyTransform(page, shape, makeShapeTransform1By2(transform));
-        api.shapeMove(page, origin, origin.indexOfChild(shape), targetParent, index++);
+        api.shapeMove(page, origin, origin.indexOfChild(shape), targetParent, index);
 
         //标记容器是否被移动到其他容器
         if (shape.parent?.isContainer && shape.parent.type !== ShapeType.Page) {
