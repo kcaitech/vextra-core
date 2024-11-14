578fda55f95b7a0e7d97486e089e14c519439aa8
Zrx <georgezrx@163.com>
2024-11-14 15:16:05 +0800

    feat.CircleChecker 40%

--- src/editor/move/circle.ts
+++ src/editor/move/circle.ts
@@ -1,31 +1,28 @@
-import { Shape, ShapeType, GroupShape, SymbolRefShape } from "../../data";
+import { ShapeView, SymbolRefView, GroupShapeView } from "../../dataview";
 
+type Side = 'start' | 'end';
+type Path = {
+    start: string;
+    end: string;
+}
 /**
- * @description 检查组件是否循环依赖
+ * @description 检查组件是否循环依赖、图层之间是否倒转父子元素关系
  */
 export class CircleChecker {
-    private static get_topology_map(shape: Shape) {
-        const deps: { shape: string, ref: string }[] = [];
-        const childs = shape.type === ShapeType.SymbolRef ? shape.naviChilds : (shape as GroupShape).childs;
-        if (!childs?.length) return [];
-        for (let i = 0, len = childs.length; i < len; i++) {
-            const item = childs[i];
-            const is_ref = item.type === ShapeType.SymbolRef
-            const c_childs = is_ref ? item.naviChilds : (item as GroupShape).childs;
-            if (c_childs?.length || is_ref) {
-                deps.push({
-                    shape: shape.type === ShapeType.SymbolRef ? (shape as SymbolRefShape).refId : shape.id,
-                    ref: is_ref ? (item as SymbolRefShape).refId : item.id
-                });
-            }
-            if (!c_childs?.length) continue;
-            deps.push(...this.get_topology_map(item));
+    private static get_topology_map(shape: ShapeView) {
+        const deps: Path[] = [];
+        const children = (shape as GroupShapeView).childs;
+        if (!children?.length) return [];
+        for (const child of children) {
+            const end = child instanceof SymbolRefView ? child.refId : child.id;
+            deps.push({ start: shape.id, end });
+            if ((child as GroupShapeView).childs?.length) deps.push(...this.get_topology_map(child));
         }
         return deps;
     }
 
-    private static filter_deps(deps: { shape: string, ref: string }[], key1: 'shape' | 'ref', key2: 'shape' | 'ref') {
-        const result: { shape: string, ref: string }[] = [];
+    private static filter_deps(deps: Path[], key1: Side, key2: Side) {
+        const result: Path[] = [];
         const _checked: Set<string> = new Set();
         const _checked_invalid: Set<string> = new Set();
         for (let i = 0, len = deps.length; i < len; i++) {
@@ -49,14 +46,14 @@ export class CircleChecker {
         return result;
     }
 
-    private static is_exist_single_stick(deps: { shape: string, ref: string }[]) {
+    private static is_exist_single_stick(deps: Path[]) {
         const set1 = new Set<string>();
         const set2 = new Set<string>();
         let is_single = false;
         for (let i = 0, len = deps.length; i < len; i++) {
             const dep = deps[i];
-            set1.add(dep.shape);
-            set2.add(dep.ref);
+            set1.add(dep.start);
+            set2.add(dep.end);
         }
         set1.forEach(v => {
             if (!set2.has(v)) is_single = true;
@@ -68,14 +65,18 @@ export class CircleChecker {
         return is_single;
     }
 
-    static assert(symbol: Shape, symbol2: string): boolean {
-        let deps: { shape: string, ref: string }[] = [
-            ...this.get_topology_map(symbol),
-            { shape: symbol2, ref: symbol.id }
-        ];
-        while (deps.length && this.is_exist_single_stick(deps)) { // 剪枝
-            deps = this.filter_deps(deps, 'shape', 'ref');
-            deps = this.filter_deps(deps, 'ref', 'shape');
+    /**
+     * @description alpha 包含 beta 即为 true 即产生了循环
+     */
+    static assert4view(alpha: ShapeView, beta: ShapeView): boolean {
+        let deps: Path[] = [...this.get_topology_map(alpha), ...this.get_topology_map(beta)];
+        const start = alpha instanceof SymbolRefView ? alpha.refId : alpha.id;
+        const end = beta instanceof SymbolRefView ? beta.refId : beta.id;
+        deps.push({ start, end });
+        console.log(JSON.parse(JSON.stringify(deps)));
+        while (deps.length && this.is_exist_single_stick(deps)) {
+            deps = this.filter_deps(deps, 'start', 'end');
+            deps = this.filter_deps(deps, 'end', 'start');
         }
         return !!deps.length;
     }
