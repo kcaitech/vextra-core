97dcc69e1fddfbbaeddbad1ab8ca61f69e11f433
Zrx <kcai@kcai.com>
2024-11-27 15:43:21 +0800

    fix.插入线条，倾斜度为45度后，复制粘贴相同线条，创建为编组后，锁定横纵比，设置宽度为12后，发现宽度会自动变为其他

--- src/editor/utils/common.ts
+++ src/editor/utils/common.ts
@@ -87,16 +87,16 @@ export function modify_shapes_height(api: Api, document: Document, page: Page, s
             continue; // 直线段的高度不可修改恒定为0.01
         }
 
-        let w = shape.size.width;
+        let w = view.frame.width;
 
-        const h = shape.size.height;
+        const h = view.frame.height;
 
         if (shape.constrainerProportions) {
             const rate = h / val;
             w = w / rate;
         }
 
-        const origin_w = shape.frame.width;
+        const origin_w = view.frame.width;
         expandTo(api, document, page, shape, w, val);
         if ((shape as Artboard).autoLayout) {
             api.shapeModifyAutoLayoutSizing(page, shape, StackSizing.Fixed, 'ver');
