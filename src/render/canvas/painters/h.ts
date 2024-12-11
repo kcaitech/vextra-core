8fa39f3b5df796fc5e53ef2295d4f7a8097dd5bc
Zrx <kcai@kcai.com>
2024-12-11 10:16:41 +0800

    refactor.canvas render 1%

--- src/render/canvas/painters/h.ts
+++ src/render/canvas/painters/h.ts
@@ -5,7 +5,6 @@ import { ShapeView } from "../../../dataview";
 export const painter: { [key: string]: (view: any, renderer: CanvasRenderer) => number } = {};
 
 painter['base'] = (view: ShapeView, renderer: CanvasRenderer) => {
-    console.log('--view--', view.name);
     // if (!renderer.checkAndResetDirty()) return renderer.m_render_version;
     renderer.renderFills();
     renderer.renderContents();
