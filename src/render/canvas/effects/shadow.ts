8fc46d99b3c87b5a5777c9a5005b5b970938eb29
Zrx <kcai@kcai.com>
2024-12-11 17:17:23 +0800

    refactor.frank shadow

--- src/render/canvas/effects/shadow.ts
+++ src/render/canvas/effects/shadow.ts
@@ -1,15 +1,23 @@
-import { Shadow } from "../../../data";
+import { Shadow, ShadowPosition } from "../../../data";
 
 export function render(ctx: CanvasRenderingContext2D, shadows: Shadow[]): Function | undefined {
-    let tail: Function | undefined = undefined;
+    if (!shadows.length) return;
+    const isComplex = shadows.length > 1 || shadows[0].spread || shadows[0].position === ShadowPosition.Inner;
+    if (isComplex) complexShadow(ctx, shadows); else return frankShadow(ctx, shadows[0]);
+}
+
+function frankShadow(ctx: CanvasRenderingContext2D, shadow: Shadow): Function {
+    ctx.save();
+    const color = shadow.color;
+    ctx.shadowColor = `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`;
+    ctx.shadowBlur = shadow.blurRadius;
+    ctx.shadowOffsetX = shadow.offsetX;
+    ctx.shadowOffsetY = shadow.offsetY;
+    return ctx.restore.bind(ctx);
+}
+
+function complexShadow(ctx: CanvasRenderingContext2D, shadows: Shadow[]) {
     for (const shadow of shadows) {
-        ctx.save();
-        const color = shadow.color;
-        ctx.shadowColor = `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`;
-        ctx.shadowBlur = shadow.blurRadius;
-        ctx.shadowOffsetX = shadow.offsetX;
-        ctx.shadowOffsetY = shadow.offsetY;
-        tail = ctx.restore.bind(ctx);
+
     }
-    return tail;
 }
