1c91d14881ff820cba01d110db3e043a33ac49cb
Kc_tjs <3288771283@qq.com>
2024-12-14 14:23:03 +0800

    背景模糊

--- src/render/canvas/painters/renderer.ts
+++ src/render/canvas/painters/renderer.ts
@@ -65,7 +65,7 @@ export class CanvasRenderer extends IRenderer {
     }
 
     renderBlur() {
-        return renderBlur(this.view);
+        return renderBlur(this.view, this.props);
     }
 
     renderContents() {
