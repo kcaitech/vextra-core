19cda516953351734578aa77aab3934a8059e5c5
张瑞强 <zhangruiqiang@192.168.0.14>
2025-02-25 14:30:42 +0800

    优化代码

--- src/schema/script/index.ts
+++ src/schema/script/index.ts
@@ -11,7 +11,20 @@ const allNodes = loadSchemas(path.join(scriptdir, '../'));
 
 genTypes(allNodes, path.join(scriptdir, '../../data/typesdefine.ts'));
 
-genClass(allNodes, path.join(scriptdir, '../../data/baseclasses.ts'));
+genClass(allNodes, path.join(scriptdir, '../../data/baseclasses.ts'), {
+    extraHeader($) {
+        $.nl('import { Basic, BasicArray, BasicMap } from "./basic"');
+    },
+    typesPath: "./typesdefine",
+    extraOrder: [
+        'GroupShape',
+    ],
+    baseClass: {
+        extends: "Basic",
+        array: "BasicArray",
+        map: "BasicMap"
+    }
+});
 
 genExp(allNodes, path.join(scriptdir, '../../data/baseexport.ts'));
 
