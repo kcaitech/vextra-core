7077fd854cfa7ee09dc3bb00bbed27b5ca79b10a
ruiqiangzh <ruiqiangzh@gmail.com>
2025-06-03 08:22:16 +0800

    导出

---
+++ src/basic/index.ts
@@ -0,0 +1,8 @@
+// 基础模块导出
+export * from "./matrix"
+export * from "./objectid"
+export * from "./utils"
+export { IEventEmitter, EventEmitter } from "./event"
+export { ColVector2D, ColVector3D } from "./matrix2"
+export { NumberArray2D } from "./number_array"
+export * from "./error" 
--- src/index.ts
+++ src/index.ts
@@ -9,217 +9,38 @@
  */
 
 import { Path } from "@kcdesign/path"
+export { Path } from "@kcdesign/path"
 import { gPal, MeasureFun, TextPathFun } from "./basic/pal"
 
-// basic
-export * from "./basic/matrix"
-export * from "./basic/objectid"
-export * from "./basic/utils"
-export { IEventEmitter, EventEmitter } from "./basic/event"
-export { ColVector2D, ColVector3D } from "./basic/matrix2"
-export { NumberArray2D } from "./basic/number_array"
+// 基础模块导出
+export * as Basic from "./basic/index"
+export * as Matrix from "./basic/matrix"
+// export * as Utils from "./basic/utils"
+export * as Event from "./basic/event"
+export * as Error from "./basic/error"
 
-export * from "./basic/error"
+// 数据模块导出
+export * as Data from "./data"
+export * as DataView from "./dataview"
 
-// data
-// 限制上层使用data,尽量使用view
-// export * from "./data"
-export {
-    IWatchable,
-    WatchableObject,
-    Basic,
-    BasicArray,
-    GradientType,
-    ShapeType,
-    BlendMode,
-    BorderPosition,
-    ImageScaleMode,
-    ExportFormatNameingScheme,
-    BlurType,
-    ShadowPosition,
-    FillType,
-    Gradient,
-    Color,
-    PaintFilter,
-    PaintFilterType,
-    Border,
-    Fill,
-    Stop,
-    Shadow,
-    CurveMode,
-    PathType,
-    TextBehaviour,
-    RadiusType,
-    Style,
-    Blur,
-    Point2D,
-    MarkerType,
-    SideType,
-    ShapeFrame,
-    ShapeSize,
-    CornerType,
-    BorderSideSetting,
-    BorderStyle,
-    ExportFormat,
-    ExportFileFormat,
-    ExportOptions,
-    ExportVisibleScaleType,
-    Text,
-    Para,
-    Variable,
-    VariableType,
-    OverrideType,
-    ContextSettings,
-    ResizingConstraints,
-    ResizingConstraints2,
-    AttrGetter,
-    TableCellType,
-    TextVerAlign,
-    TextHorAlign,
-    UnderlineType,
-    StrikethroughType,
-    TextTransformType,
-    CurvePoint,
-    ContactForm,
-    BulletNumbersType,
-    BulletNumbers,
-    PageListItem,
-    GuideAxis,
-    BoolOp,
-    ContactType,
-    SpanAttr,
-    TextAttr,
-    FillRule,
-    PatternTransform,
+// IO模块导出
+export * as IO from "./io"
+export * as Import from "./io/import"
+export * as Export from "./io/export"
+export * as Clipboard from "./io/cilpboard"
+export * as SvgParser from "./io/svg_parser"
 
-    TableGridItem,
-    TableLayout,
-    ResourceMgr,
+// 编辑器模块导出
+export * as Editor from "./editor"
 
-    // todo
-    Shape,
-    Page,
-    TextShape,
-    SymbolShape,
-    SymbolRefShape,
-    TableCell,
-    CutoutShape,
-    GroupShape,
-    Artboard,
-    SymbolUnionShape,
-    TableShape,
-    ContactShape,
-    BoolShape,
-    PathShape,
-    PolygonShape,
-    RectShape,
-    ImageShape,
-    StarShape,
+// 协作模块导出
+export * as Coop from "./coop"
 
-    Document,
-    TransactDataGuard,
+// 工具函数导出
+export { exportBorder, exportFill, exportShadow, exportBlur, exportContextSettings } from './data/baseexport'
+export { convertGetFileResponse } from "./figmcpconvert/index"
 
-    AutoLayout,
-    StackMode,
-    StackAlign,
-    StackSizing,
-    StackWrap,
-    StackSize,
-    OverlayPosition,
-    OverlayMargin,
-    OverlayPositionType,
-    OverlayBackgroundType,
-    OverlayBackgroundInteraction,
-    OverlayBackgroundAppearance,
-
-    PrototypeStartingPoint,
-    PrototypeInteraction,
-    PrototypeConnectionType,
-    PrototypeEasingType,
-    PrototypeActions,
-    PrototypeEvents,
-    PrototypeEvent,
-    PrototypeNavigationType,
-    PrototypeTransitionType,
-    PrototypeEasingBezier,
-
-    ScrollDirection,
-    ScrollBehavior,
-
-    StackPositioning,
-
-    parsePath,
-    ShadowMask,
-    FillMask,
-    BlurMask,
-    StyleMangerMember,
-    CornerRadius,
-    PathSegment,
-    BorderMask,
-    BorderMaskType,
-    StyleSheet,
-    RadiusMask,
-    TextMask
-} from "./data"
-export * from "./data/shapedirlist"
-export * from "./data/shapedirlist2"
-
-export {
-    importGradient
-} from "./data"
-
-// data view
-export * from "./dataview"
-
-// io
-export * from "./io"
-export * from "./io/import"
-export * from "./io/export"
-export { exportSvg } from "./io/export/svg";
-export { exportImg } from "./io/export/img";
-// export * from "./io/import/sketch/lzdata"
-export * from "./io/cilpboard"
-// export
-export * from "./io/export/svg"
-export * as svgParser from "./io/svg_parser";
-
-// editor
-export * from "./editor"
-export * from "./editor/document"
-export * from "./editor/page"
-export * from "./editor/shape"
-export * from "./editor/textshape"
-export * from "./editor/table"
-export * from "./editor/controller"
-export * as creator from "./editor/creator/creator"
-
-
-export * from "./editor/asyncapi"
-
-// coop
-export { PaddingDir } from "./coop/recordop"
-export * from "./coop"
-
-export { Transform } from "./data/transform"
-
-// properties
-export { exportBorder, exportFill, exportShadow, exportBlur, exportContextSettings } from './data/baseexport';
-
-export { LinearApi } from './editor/linearapi/linearapi';
-
-export { CircleChecker } from "./editor/basic/move/circle";
-
-export { Path } from '@kcdesign/path'
-
-export { FillModifier } from "./editor/style/fill";
-export { BorderModifier } from "./editor/style/border";
-export { RadiusModifier } from "./editor/style/radius";
-export { ShadowsModifier } from "./editor/style/shadows";
-export { BlurModifier } from "./editor/style/blur";
-export { TextModifier } from "./editor/style/text";
-
-export { convertGetFileResponse } from "./figmcpconvert/index";
-
+// 初始化函数
 export async function initModule(textMeasure: MeasureFun, text2path: TextPathFun) {
     gPal.text.textMeasure = textMeasure
     gPal.text.getTextPath = text2path
