52a819f6f4d995d9a394f945a3d36e55b0f2b016
ruiqiangzh <ruiqiangzh@gmail.com>
2025-06-03 08:22:16 +0800

    导出

--- src/editor/index.ts
+++ src/editor/index.ts
@@ -8,18 +8,6 @@
  * https://www.gnu.org/licenses/agpl-3.0.html
  */
 
-import { Document } from "../data/document";
-import { DocEditor } from "./document";
-import { PageEditor } from "./page";
-import { ShapeEditor } from "./shape";
-import { Controller } from "./controller"; /* 逐步取消Controller */
-import { CoopRepository } from "../coop/cooprepo";
-import { TextShapeEditor } from "./textshape";
-import { TableEditor } from "./table";
-import { resizingConstraintEditor } from "./resizingConstraint";
-import { ISave4Restore } from "../coop/localcmd";
-import { PageView, ShapeView, TableCellView, TableView, TextShapeView } from "../dataview";
-import { Page } from "../data/page";
 
 export * from "./utils/auto_layout"
 export * from "./utils/auto_layout2"
@@ -31,79 +19,18 @@ export { RefUnbind } from "./symbol"
 
 export { ContactLineModifier } from "./contact";
 
-export class Editor {
-    private m_data: Document;
-    // private m_selection: Selection;
-    private m_repo: CoopRepository;
-    // private m_shadows: ShapeNaviShadowMgr | undefined;
-    private m_docEditor: DocEditor | undefined;
-    // private m_pageEditors: Map<string, PageEditor> = new Map();
+export * from "./editor"
+// 编辑器核心功能
+export * from "./document"
+export * from "./page"
+export * from "./controller"
+export * from "./asyncapi"
 
-    constructor(data: Document, repo: CoopRepository, selection: ISave4Restore) {
-        this.m_data = data;
-        // this.m_selection = selection;
-        this.m_repo = repo;
-    }
+// 编辑器图形功能
+export * from "./shape"
+export * from "./textshape"
+export * from "./table"
+export * from "./creator/creator"
 
-    editor4Doc(): DocEditor {
-        if (this.m_docEditor === undefined) {
-            this.m_docEditor = new DocEditor(this.m_data, this.m_repo);
-        }
-        return this.m_docEditor;
-    }
-
-    editor4Page(page: PageView): PageEditor {
-        // let e = this.m_pageEditors.get(page.id);
-        // if (e) {
-        //     return e;
-        // }
-        // e = new PageEditor(this.m_repo, page.data, this.m_data);
-        // this.m_pageEditors.set(page.id, e);
-        // return e;
-        if ((!page.data) || (!(page.data instanceof Page))) throw Error("page.data is not Page!")
-        return new PageEditor(this.m_repo, page, this.m_data);
-    }
-
-    editor4Shape(page: PageView, shape: ShapeView): ShapeEditor {
-        // get page
-        // const p: ShapeView | undefined = shape.getPage();
-        // if ((!p) || (!(p instanceof PageView))) throw Error("shape has not parent Page!")
-        // const pe = this.editor4Page(p);
-        // return pe.editor4Shape(shape);
-        return new ShapeEditor(shape, page, this.m_repo, this.m_data);
-    }
-
-    editor4TextShape(page: PageView, shape: TextShapeView | TableCellView): TextShapeEditor {
-        // get page
-        // const p: ShapeView | undefined = shape.getPage();
-        // if ((!p) || (!(p instanceof PageView))) throw Error("shape has not parent Page!")
-        // const pe = this.editor4Page(p);
-        // return pe.editor4TextShape(shape);
-        return new TextShapeEditor(shape, page, this.m_repo, this.m_data);
-    }
-    editor4Table(page: PageView, shape: TableView): TableEditor {
-        // get page
-        // const p: ShapeView | undefined = shape.getPage();
-        // if ((!p) || (!(p instanceof PageView))) throw Error("shape has not parent Page!")
-        // const pe = this.editor4Page(p);
-        // return pe.editor4Table(shape);
-        return new TableEditor(shape, page, this.m_repo, this.m_data);
-    }
-    controller(): Controller {
-        const e = new Controller(this.m_repo, this.data);
-        return e;
-    }
-    editor4ResizingConstraint(page: Page) {
-        return new resizingConstraintEditor(page, this.m_repo, this.data);
-    }
-
-    get data() {
-        return this.m_data;
-    }
-    get repo() {
-        return this.m_repo;
-    }
-    // get selection() {
-    //     return this.m_selection;
-    // }
-}
+// 编辑器样式功能
+export * from "./style"
