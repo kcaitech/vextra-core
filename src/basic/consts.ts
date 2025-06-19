550fe21fef99e737f8665ad26b3001dc26eab059
张瑞强 <zhangruiqiang@kcaitech.com>
2025-06-19 11:14:59 +0800

    支持nodejs环境用路径打开文档
    (cherry picked from commit 6c969ea1)

--- src/basic/consts.ts
+++ src/basic/consts.ts
@@ -9,3 +9,7 @@
  */
 
 export const float_accuracy = 1e-7;
+
+// 环境检测
+export const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+export const isNode = typeof process !== 'undefined' && process.versions && process.versions.node;
